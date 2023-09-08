90b370e785307b272a619d8f84e9e8289b58fb02
Suracheth Chawla <me@suracheth.com>
2023-09-08 20:07:15 +0700

    Reduce minimum elixir version to 1.11 to match the minimum elixir version in `finch` library

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plausible.MixProject do
     [
       app: :plausible,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
